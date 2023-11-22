c6befa175d508049e6b172c5db02e72e7400d2e6
Nathan <antnefedov33@gmail.com>
2023-11-22 19:13:04 +0300

    wrote script to paint items, changed some styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,24 @@
 "use strict"
+
+let slider = document.querySelector('.slider');
+let items = document.querySelectorAll('.item');
+
+paintItems(items);
+
+function paintItems(array) {
+    for (let item of array) {
+        item.style.background = createRGB();
+    }
+}
+
+function createRGB() {
+    let red = getRandomNumber(0, 255);
+    let green = getRandomNumber(0, 255);
+    let blue = getRandomNumber(0, 255);
+
+    return `rgba(${red}, ${green}, ${blue}, 0.9)`;
+};
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
